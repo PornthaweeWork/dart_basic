d8bdef3269e0d1e6b011038a2c923aeb9d911290
PornthaweeWork <pornthawee.work@gmail.com>
2023-01-04 17:15:27 +0700

    type variable

--- bin/dart_basic.dart
+++ bin/dart_basic.dart
@@ -1,4 +1,17 @@
 void main() {
-  const x = 10;
-  
+  var y = 10;
+  var x = 'Hello World ${y.round()}';
+  print(x);
+
+  dynamic z;
+  z = 10;
+  z = 'Hello';
+
+  var numbers = <int>[1,2,3];
+
+  for (var n in numbers) {
+    print(n);
+  }
+
+
 }
