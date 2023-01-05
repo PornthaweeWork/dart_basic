51bdf315b85f3c1ed719d5fffb4effe99923f17c
PornthaweeWork <pornthawee.work@gmail.com>
2023-01-05 13:30:14 +0700

    Use Function

--- bin/dart_basic.dart
+++ bin/dart_basic.dart
@@ -1,7 +1,7 @@
 void main() {
   var y = 10;
   var x = 'Hello World ${y.round()}';
-  print(x);
+  // print(x);
 
   dynamic z;
   z = 10;
@@ -12,7 +12,7 @@ void main() {
   numbers[0] = 10;
 
   for (var n in numbers) {
-    print(n);
+    // print(n);
   }
 
   var countriesList = ['Thailand','United State'];
@@ -21,8 +21,8 @@ void main() {
   countriesList.add('Thailand');
   countries.add('Thailand');
 
-  print(countriesList);
-  print(countries);
+  // print(countriesList);
+  // print(countries);
 
   var countriesObject = <String, Object>{
     'name': 'Bond',
@@ -30,15 +30,15 @@ void main() {
   };
 
   for (var c in countriesObject.keys) {
-    print(c);
+    // print(c);
   }
 
   for (var c in countriesObject.values) {
-    print(c);
+    // print(c);
   }
 
   for (var c in countriesObject.entries) {
-    print(c);
+    // print(c);
   }
 
   var isMan = true;
@@ -49,18 +49,39 @@ void main() {
     for (var n in intern) n,
     ...intern,
   ];
-  print(names);
+  // print(names);
 
   var a = [1, 2, 3];
   var b = [...a];
 
   a[0] = 10;
-  print(a);
-  print(b);
+  // print(a);
+  // print(b);
 
   var m = Days.monday;
-  print(m);
+  // print(m);
+
+ final o =  helloWorld(name: 'Obey');
+//  print(o);
+
+ final g = sum(10, 20);
+//  print(g);
+
+  final f = (int a, int b) => a + b;
+  var result = f(10, 20);
+  print(result);
+
+  result = cal(10, 20, f);
 }
+
+int cal(int a, int b, Calculator f) => f(a, b);
+
+typedef Calculator = int Function(int, int);
+
+String helloWorld({String name = '', int? age}) => 'Hello World $name $age';
+
+int sum(int a, int b) => a + b;
+
 
 enum Days {
   monday, tuesday, wednesday
